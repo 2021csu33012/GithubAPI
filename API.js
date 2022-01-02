0185f8bba758a0cc696567bb65c74b3e6f0506ee
2021csu33012 <91067291+2021csu33012@users.noreply.github.com>
2022-01-02 22:24:26 +0000

    Updated code
    Added code that divides number of followers by push events to get an average number of followers gained per push

--- API.js
+++ API.js
@@ -1,4 +1,10 @@
 function requestRepos(username){
+    const reqEvents = new XMLHttpRequest();
+    var pushEvents = 0;
+    var eventType = 'PushEvent';
+    var followerCount = 0;
+    const urlEvents = `https://api.github.com/users/${username}/events`;
+    reqEvents.open('GET', urlEvents, true);
     const reqFull = new XMLHttpRequest();
     const req = new XMLHttpRequest();
     const reqFollowers = new XMLHttpRequest();
@@ -26,12 +32,28 @@ function requestRepos(username){
 
 
             console.log('Followers: ', fullData.followers);
+            followerCount = fullData.followers;
             if (fullData.following != null){
             console.log('Following: ', fullData.following);
             }
     }
 
     reqFull.send();
+
+    reqEvents.onload = function(){
+        const eventData = JSON.parse(this.response);
+        console.log(eventData);
+
+        for (let i in eventData){
+            if (eventData[i].type == eventType){
+                pushEvents++;
+            }
+        }
+        console.log('Pushes: ', pushEvents);
+        console.log('Average followers per push: ', followerCount/pushEvents);
+    }
+
+    reqEvents.send();
 
 }
 
