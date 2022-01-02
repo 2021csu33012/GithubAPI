d8c3c9f7dbfae923b6efd0b4ff1eba782448e5de
2021csu33012 <91067291+2021csu33012@users.noreply.github.com>
2022-01-02 13:45:51 +0000

    Gets custom info
    Gets number of followers and number of users followed

--- API.js
+++ API.js
@@ -1,7 +1,16 @@
 function requestRepos(username){
+    const reqFull = new XMLHttpRequest();
     const req = new XMLHttpRequest();
+    const reqFollowers = new XMLHttpRequest();
+    const reqFollowing = new XMLHttpRequest();
+    const urlFull = `https://api.github.com/users/${username}`;
     const url = `https://api.github.com/users/${username}/repos`;
+    const urlFollowers = `https://api.github.com/users/${username}/followers`;
+    const urlFollowing = `https:api.github.com/users/${username}/following`;
+    reqFull.open('GET', urlFull, true);
     req.open('GET', url, true);
+    reqFollowers.open('GET', urlFollowers, true);
+    reqFollowing.open('GET', urlFollowing, true);
 
     req.onload = function(){
         const data = JSON.parse(this.response);
@@ -10,6 +19,20 @@ function requestRepos(username){
     }
 
     req.send();
+
+    reqFull.onload = function(){
+        const fullData = JSON.parse(this.response);
+        console.log(fullData);
+
+        for (let i in fullData){
+            console.log('Followers: ', fullData.followers);
+            if (fullData.following != null){
+            console.log('Following: ', fullData.following);
+            }
+        }
+    }
+
+    reqFull.send();
 
 }
 
