0acb84e2026280122088ef11d99d8efea1d219be
2021csu33012 <91067291+2021csu33012@users.noreply.github.com>
2022-01-02 23:43:07 +0000

    Updated code
    Started work on functionality of webpage

---
+++ application.js
@@ -0,0 +1,65 @@
+const githubForm = document.getElementById('githubForm');
+githubForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    let userInput = document.getElementById('userInput');
+    let githubUser = userInput.value;
+    requestInfo(githubUser);
+})
+
+function requestInfo(username){
+    const reqEvents = new XMLHttpRequest();
+    var pushEvents = 0;
+    var eventType = 'PushEvent';
+    var followerCount = 0;
+    const urlEvents = `https://api.github.com/users/${username}/events`;
+    reqEvents.open('GET', urlEvents, true);
+    const reqFull = new XMLHttpRequest();
+    const req = new XMLHttpRequest();
+    const reqFollowers = new XMLHttpRequest();
+    const reqFollowing = new XMLHttpRequest();
+    const urlFull = `https://api.github.com/users/${username}`;
+    const url = `https://api.github.com/users/${username}/repos`;
+    const urlFollowers = `https://api.github.com/users/${username}/followers`;
+    const urlFollowing = `https:api.github.com/users/${username}/following`;
+    reqFull.open('GET', urlFull, true);
+    req.open('GET', url, true);
+    reqFollowers.open('GET', urlFollowers, true);
+    reqFollowing.open('GET', urlFollowing, true);
+
+    req.onload = function(){
+        const data = JSON.parse(this.response);
+        console.log(data);
+
+    }
+
+    req.send();
+
+    reqFull.onload = function(){
+        const fullData = JSON.parse(this.response);
+        console.log(fullData);
+
+
+            console.log('Followers: ', fullData.followers);
+            followerCount = fullData.followers;
+            if (fullData.following != null){
+            console.log('Following: ', fullData.following);
+            }
+    }
+
+    reqFull.send();
+
+    reqEvents.onload = function(){
+        const eventData = JSON.parse(this.response);
+        console.log(eventData);
+
+        for (let i in eventData){
+            if (eventData[i].type == eventType){
+                pushEvents++;
+            }
+        }
+        console.log('Pushes: ', pushEvents);
+        console.log('Average followers per push: ', followerCount/pushEvents);
+    }
+
+    reqEvents.send();
+}
