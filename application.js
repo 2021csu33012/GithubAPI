0c1ed99b3e6d34411e65478f1b45cd2dc51228d3
2021csu33012 <91067291+2021csu33012@users.noreply.github.com>
2022-01-03 19:17:01 +0000

    Updated Code

--- application.js
+++ application.js
@@ -1,4 +1,6 @@
 const githubForm = document.getElementById('githubForm');
+//const output = document.querySelector('output');
+var averageFollower = 0;
 githubForm.addEventListener('submit', (e)=> {
     e.preventDefault();
     let userInput = document.getElementById('userInput');
@@ -7,11 +9,12 @@ githubForm.addEventListener('submit', (e)=> {
 })
 
 function requestInfo(username){
+    e.preventDefault();
     const reqEvents = new XMLHttpRequest();
     var pushEvents = 0;
     var eventType = 'PushEvent';
     var followerCount = 0;
-    const urlEvents = `https://api.github.com/users/${username}/events`;
+    const urlEvents = `https://api.github.com/users/${username}/events?per_page=100`;
     reqEvents.open('GET', urlEvents, true);
     const reqFull = new XMLHttpRequest();
     const req = new XMLHttpRequest();
@@ -19,7 +22,7 @@ function requestInfo(username){
     const reqFollowing = new XMLHttpRequest();
     const urlFull = `https://api.github.com/users/${username}`;
     const url = `https://api.github.com/users/${username}/repos`;
-    const urlFollowers = `https://api.github.com/users/${username}/followers`;
+    const urlFollowers = `https://api.github.com/users/${username}/followers?per_page=100`;
     const urlFollowing = `https:api.github.com/users/${username}/following`;
     reqFull.open('GET', urlFull, true);
     req.open('GET', url, true);
@@ -46,6 +49,7 @@ function requestInfo(username){
             }
     }
 
+
     reqFull.send();
 
     reqEvents.onload = function(){
@@ -59,7 +63,19 @@ function requestInfo(username){
         }
         console.log('Pushes: ', pushEvents);
         console.log('Average followers per push: ', followerCount/pushEvents);
+        //const res = followerCount/pushEvents;
+        const res = 9;
+        <p><strong>Average amount of followers per push</strong> $ {followerCount/pushEvents}</p>
+        output.textContent = `Result: ${res}`;
+        document.getElementById('output').innerHTML = "Average amount of followers earned per push: " + followerCount/pushEvents;
+        return res;
     }
 
     reqEvents.send();
+}
+
+function displayRes(){
+    var result = requestInfo(username);
+    document.getElementById('output').innerHTML = result;
+    return false;
 }
